57ca7a0741b9d8a253789c5731fdf607dad4c3df
Ramish-Amir <49115590+Ramish-Amir@users.noreply.github.com>
2022-02-09 23:21:49 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# atomonetworks
+ Web Application
---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+var navLinks = document.getElementById("navLinks");
+var myNav = document.getElementById("myNav");
+function openMenu() {
+    console.log('menu open')
+    navLinks.style.right = "0";
+}
+
+function closeMenu() {
+    console.log('menu close')
+    navLinks.style.right = "-250px";
+}
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop >= 100) {
+        myNav.classList.add("nav-colored");
+        navLinks.classList.add("nav-links-colored")
+    }
+    else {
+        myNav.classList.remove("nav-colored");
+        navLinks.classList.remove("nav-links-colored")
+    }
+};
