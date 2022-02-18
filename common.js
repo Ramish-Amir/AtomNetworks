a2ebd478fd6d2e317a971c1d8be00bc10e1ff5fc
Ramish-Amir <49115590+Ramish-Amir@users.noreply.github.com>
2022-02-18 14:42:49 +0500

    Added Internet pages for Home and Business

--- common.js
+++ common.js
@@ -21,3 +21,31 @@ window.onscroll = function () {
         navLinks.classList.remove("nav-links-colored")
     }
 };
+
+function searchOffer() {
+    var searchString = ((document.getElementById('searchInput')).value).toUpperCase();
+    var offers = document.getElementsByClassName('card');
+    for (let i=0; i<offers.length; i++) {
+        if (!(offers[i].childNodes[1].innerText.toUpperCase()).includes(searchString)) {
+            offers[i].style.display = "none"
+        } else {
+            offers[i].style.display = "block"
+        }
+        
+    }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active-accordion");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
