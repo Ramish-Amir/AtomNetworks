515988b2d055d3700a103440edb7e25577a1f8b9
Ramish-Amir <49115590+Ramish-Amir@users.noreply.github.com>
2022-02-23 01:11:50 +0500

    Implemented smtp API integration

--- common.js
+++ common.js
@@ -25,13 +25,13 @@ window.onscroll = function () {
 function searchOffer() {
     var searchString = ((document.getElementById('searchInput')).value).toUpperCase();
     var offers = document.getElementsByClassName('card');
-    for (let i=0; i<offers.length; i++) {
+    for (let i = 0; i < offers.length; i++) {
         if (!(offers[i].childNodes[1].innerText.toUpperCase()).includes(searchString)) {
             offers[i].style.display = "none"
         } else {
             offers[i].style.display = "block"
         }
-        
+
     }
 }
 
@@ -39,13 +39,38 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active-accordion");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active-accordion");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+function sendEmail() {
+    const fName = document.getElementById('fName').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const msg = document.getElementById('userMessage').value;
+
+    if (fName === null || fName === '' || email === null || email === '' || msg === '' || msg === null) {
+        alert("Kindly fill all the required fields!")
     }
-  });
+    else {
+        Email.send({
+            SecureToken: 'dummy_secure_token',
+            To: 'dummyEmail@gmail.com',
+            From: email,
+            Subject: "User Enquiry",
+            Body: "Name: " + fName
+                + "<br> Email: " + email
+                + "<br> Phone no: " + phone
+                + "<br> Message: " + msg
+        }).then(
+            message => alert(message)
+        );
+    }
 }
